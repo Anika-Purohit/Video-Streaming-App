66931c360bae7b7f34014012f4445c301b947770
Anika-Purohit <anikapurohit6@gmail.com>
2023-04-25 00:27:58 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const CURR_KEY = "AIzaSyAkhV5C2mMWmttY43c91AdeaAJ0iIkuokk";
 
-export const YOUTUBE_API_KEY = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key="+CURR_KEY;
+export const YOUTUBE_API_KEY = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+CURR_KEY;
 
 export const JSON_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
