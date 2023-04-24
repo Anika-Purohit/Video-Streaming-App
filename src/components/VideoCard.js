5f16bfd92654bea1c60b73ebffd83d33e19b1484
Anika-Purohit <anikapurohit6@gmail.com>
2023-04-24 16:17:51 +0530

    .

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -6,12 +6,12 @@ const VideoCard = ({info}) => {
   const {channelTitle,title,thumbnails} = snippet;  
  
   return (  
-  <div className=' w-96  h-[365px] rounded-lg'> 
+  <div className='  h-[365px] rounded-lg'> 
    <img class="rounded-lg  " alt="thumbnail" src= {thumbnails.maxres.url}/>
   <ul>
     <li className='p-1 font-bold'>{title}</li>
-    <li className='px-1 text-slate-600'>{channelTitle}</li>
-    <li className='px-1 text-slate-600'>{statistics.viewCount} views</li>
+    <li className='px-1 text-stone-500'>{channelTitle}</li>
+    <li className='px-1 text-stone-500'>{statistics.viewCount} views</li>
   </ul>
   </div> 
   );
