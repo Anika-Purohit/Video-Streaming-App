d09a8e006a4cd8c3c5e70a9cb68746e29e668dd9
Anika-Purohit <anikapurohit6@gmail.com>
2023-08-31 14:25:33 +0530

    final logo change/ api change

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,9 +1,27 @@
-import React from 'react'
-
 const Shimmer = () => {
-  return (
-    <div className='text-3xl'> Shimmer</div>
-  )
-}
-
-export default Shimmer
+      return (
+      
+          <div className=" p-2 flex flex-wrap justify-center animate-pulse m-2 ">
+            {Array(20).fill("").map((e, index) => {
+                return (
+                  <div className="w-60 h-80 p-2 m-7 rounded-2xl bg-stone-100  flex flex-wrap" key={index} >
+                    <div className="flex-1 space-y-5 py-1">
+                      <div className="h-40 bg-slate-200 rounded"></div>
+                       <div className="space-y-3">
+                        <div className="grid grid-cols-3 gap-4">
+                          <div className="h-2 bg-slate-200 rounded col-span-1"></div>
+                          <div className="h-2 bg-slate-200 rounded col-span-1"></div>
+                          <div className="h-2 bg-slate-200 rounded col-span-1"></div>
+                        </div>
+                        <div className="h-2 bg-slate-200 rounded"></div>
+                       
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        
+      );
+    };
+ export default Shimmer
