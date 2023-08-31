4de6c2f5a0785f23b1707e4ffa413a6b0a3b3c89
Anika-Purohit <anikapurohit6@gmail.com>
2023-08-31 14:41:58 +0530

    ,,

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,6 +2,6 @@ const CURR_KEY = "AIzaSyAkhV5C2mMWmttY43c91AdeaAJ0iIkuokk";
 
 export const YOUTUBE_API_KEY = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key="+CURR_KEY;
 
-export const JSON_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const JSON_SEARCH_API = "https://corsproxy.io/?https://clients1.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const OFFSET_LIVE_CHAT = 30;
